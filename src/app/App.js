1217f36a7c2863feaeae169cd34b25d67a16e9c9
Gang Yue <gang.yue@thoughtworks.com>
2020-08-16 22:27:14 +0800

    [yuegang] feat: finish task-1

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './app.less';
-import {Route, BrowserRouter} from "react-router-dom";
+import {Route, BrowserRouter, NavLink, Switch} from "react-router-dom";
 import Home from "./Home";
 
 class App extends Component {
@@ -8,7 +8,19 @@ class App extends Component {
     return (
       <div className="app">
         <BrowserRouter>
-          <Route exact path='/' component={Home}/>
+          <nav className="header">
+            <ul>
+              <li><NavLink exact to="/">HOME</NavLink></li>
+              <li><NavLink exact to="/timer" >在线倒计时器</NavLink></li>
+              <li><NavLink exact to="/calculate">在线计算器</NavLink></li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route exact path="/calculate" component={Home}></Route>
+            <Route exact path="/timer" component={Home}></Route>
+            <Route exact path="/" component={Home}></Route>
+          </Switch>
+          
         </BrowserRouter>
       </div>
     );
--- src/app/Home.js
+++ src/app/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import './home.less';
-
+import hero from '../images/hero-image.png';
+import claPic from '../images/calculator.png';
+import timerPic from '../images/timer.png';
+import { NavLink } from 'react-router-dom';
 const Home = () => {
   return (<div className="home">
-    This is home page
+    <div><img className="heroPic" src={hero}></img></div>
+    <div className="body">
+      <div className = 'textCenter'>
+        <NavLink to='/calculate'><img className="claPic" src={claPic}></img></NavLink>
+        <NavLink to='/calculate'>在线计算器</NavLink>
+      </div>
+      <div className = 'textCenter'>
+        <NavLink to='/timer'><img className="timerPic" src={timerPic}></img></NavLink>
+        <NavLink to='/timer'>在线倒计时器</NavLink>
+      </div>
+    </div>
   </div>);
 };
 
